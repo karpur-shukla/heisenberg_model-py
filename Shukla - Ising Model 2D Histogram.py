f5761bf99858ba43390f616d6ab69bc2a6492246
Karpur Shukla <k-shukla@users.noreply.github.com>
2017-07-04 22:19:13 -0400

    Update Shukla - Ising Model 2D Histogram.py

--- Shukla - Ising Model 2D Histogram.py
+++ Shukla - Ising Model 2D Histogram.py
@@ -27,20 +27,24 @@ program_start_time = time.clock()
 
 
 # This section sets the simulation parameters.
-x_len = 8              # x_len is the number of sites in each row.
-y_len = 8              # y_len is the number of rows in each column.
-size = x_len * y_len   # size simply keeps the total number of sites handy.
+x_len = 8                    # x_len is the number of sites in each row.
+y_len = 8                    # y_len is the number of rows in each column.
+size = x_len * y_len         # size simply keeps the total number of sites handy.
 
-MC_num = 1000000       # MC_num is the number of Monte Carlo updates.
-sweeps = 100           # sweeps is the number of parameter sweeps.
-hist_bin_size = 1      # hist_bin_size is the size of the bins of the histograms.
-MC_therm_steps = 10000 # MC_therm_steps is the number of initial thermalisation steps.
+MC_num = 1000000             # MC_num is the number of Monte Carlo updates.
+hist_bin_size = 1            # hist_bin_size is the size of the bins of the histograms.
+MC_therm_steps = 10000       # MC_therm_steps is the number of thermalisation steps.
 
-h_hist = 0.0           # h_hist is the histogram external field.
-T_hist = 2.5           # T_hist is the histogram temperature.
-b_hist = 1/T_hist      # b_hist is the value of beta corresponding to the histogram temperature.
-Jx_hist = 1.0          # Jx_hist is the histogram x-direction coupling constant.
-Jy_hist = 1.0          # Jy_hist is the histogram y-direction coupling constant.
+h_hist = 0.0                 # h_hist is the histogram external field.
+
+T_hist = 2.5                 # T_hist is the histogram temperature.
+b_hist = 1/T_hist            # b_hist is the value of beta corresponding to the histogram temperature.
+
+Jx_hist = 1.0                # Jx_hist is the histogram x-direction coupling constant.
+Jy_hist = 1.0                # Jy_hist is the histogram y-direction coupling constant.
+
+kNN_2pt_G_dist = 1           # kNN_2pt_G_dist is the distance at which we're looking at the kth nearest-neighbour two-point Green function.
+kNN_2pt_G_conn = False       # kNN_2pt_G_conn tells us whether we're looking at the two-point disconnected or the two-point connected Green function.
 
 
 # This section creates the initial system, a static 2D array of spins (up or down).
@@ -101,25 +105,46 @@ def MC_update(lat, h, Jx, Jy, T):
     single line. '''
 
 
-# This function retrieves the magnetisation and the energy.
-def lat_props(trel, mu, ccx, ccy, temp):
+# This function retrieves the magnetisation, energy, and kth nearest-neighbour two-point Green function of a given lattice.
+def lat_props(trel, mu, ccx, ccy, temp, dist, conn):
     net_M = 0.0
     net_E = 0.0
-    array_x_size = len(trel[0])
-    array_y_size = len(trel)
-    sites = float(array_x_size * array_y_size)
+    net_corr = 0.0
+    
+    x_size = len(trel[0])
+    y_size = len(trel)
+    
+    sites = float(x_size * y_size)
 
-    for y_pt in xrange(array_y_size):
-        for x_pt in xrange(array_x_size):
-            net_M += trel[y_pt][x_pt]
-            net_E += -mu * trel[y_pt][x_pt]
-            net_E += -ccx * trel[y_pt][(x_pt+1) % array_x_size] * trel[y_pt][x_pt]
-            net_E += -ccy * trel[(y_pt+1) % array_y_size][x_pt] * trel[y_pt][x_pt]
-
-    lat_m = net_M/sites
-    lat_e = net_E/sites
-
-    return (net_M, lat_m, net_E, lat_e)
+    for y_pt in xrange(y_size):
+        for x_pt in xrange(x_size):
+            curr_site = trel[y_pt][x_pt]
+            next_site_down = trel[(y_pt + dist) % y_size][x_pt]
+            next_site_right = trel[y_pt][(x_pt + dist) % x_size]
+            
+            net_M += curr_site
+            
+            net_E += -mu * curr_site
+            net_E += -ccx * trel[y_pt][(x_pt + 1) % x_size] * curr_site
+            net_E += -ccy * trel[(y_pt + 1) % y_size][x_pt] * curr_site
+            
+            net_corr += curr_site * next_site_down
+            net_corr += curr_site * next_site_right
+            
+    
+    lat_m = net_M / sites
+    lat_e = net_E / sites
+    disc_corr_func = net_corr / sites
+    conn_corr_func = disc_corr_func - (lat_m ** 2.0)
+    
+    if conn == True:
+        return (net_M, lat_m, net_E, lat_e, conn_corr_func)
+    
+    elif conn == False:
+        return (net_M, lat_m, net_E, lat_e, disc_corr_func)
+    
+    else:
+        raise TypeError("'conn' must be of type bool")
 
 
 # This function performs the MC thermalisation.
@@ -133,9 +158,10 @@ def MC_thermal(collec, therm_steps, mag_field, couplx, couply, t):
 
 
 # This function performs several Monte Carlo updates, with the number of Monte Carlo updates specified by MC_iter.
-def many_MC(array, MC_iter, ext_field, cc_x, cc_y, tepl):
+def many_MC(array, MC_iter, ext_field, cc_x, cc_y, tepl, therm_steps_per_sample, many_MC_G_dist, many_MC_G_corr):
     MC_M = [0] * MC_iter
     MC_E = [0] * MC_iter
+    MC_G = [0] * MC_iter
     
     x_dist = len(array[0])
     y_dist = len(array)
@@ -146,35 +172,38 @@ def many_MC(array, MC_iter, ext_field, cc_x, cc_y, tepl):
     now_lat = array
     
     for update in xrange(MC_iter):
+        now_lat = MC_thermal(now_lat, therm_steps_per_sample, ext_field, cc_x, cc_y, tepl)
         now_update = MC_update(now_lat, ext_field, cc_x, cc_y, tepl)
-        now_props = lat_props(now_update, ext_field, cc_x, cc_y, tepl)
+        now_props = lat_props(now_update, ext_field, cc_x, cc_y, tepl, many_MC_G_dist, many_MC_G_corr)
         now_lat = now_update
         
         MC_M[update] = now_props[0]
         MC_E[update] = now_props[2]
+        MC_G[update] = now_props[4]
     
     avg_M = numpy.mean(MC_M, axis = None)
     avg_m = float(avg_M / points)
     avg_E = numpy.mean(MC_E, axis = None)
     avg_e = float(avg_E / points)
+    avg_G = numpy.mean(MC_G, axis = None)
+    
     sus = b * numpy.var(MC_M, axis = None) / points
     cv = math.pow(b, 2) * numpy.var(MC_E, axis = None) / points
-
-    return (now_lat, avg_M, avg_m, avg_E, avg_e, sus, cv, MC_M, MC_E)
+    
+    return (now_lat, avg_M, avg_m, avg_E, avg_e, avg_G, sus, cv, MC_M, MC_E, MC_G)
 
 
 # This section creates and plots the histograms of the average total energy and average total magnetisation of each generated lattice.
-def MC_hist(grid, MC_steps, mag_mom, coupl_x, coupl_y, tymherr, bin_size, hist_therm_steps):
-    thermalised_grid = MC_thermal(grid, hist_therm_steps, mag_mom, coupl_x, coupl_y, tymherr)
-    MC_results = many_MC(thermalised_grid, MC_steps, mag_mom, coupl_x, coupl_y, tymherr)
+def MC_hist(grid, MC_steps, mag_mom, coupl_x, coupl_y, tymherr, bin_size, hist_therm_steps, MC_hist_G_dist, MC_hist_G_corr):
+    MC_results = many_MC(grid, MC_steps, mag_mom, coupl_x, coupl_y, tymherr, hist_therm_steps, MC_hist_G_dist, MC_hist_G_corr)
 
-    MC_int_M_array = numpy.rint(MC_results[7])
+    MC_int_M_array = numpy.rint(MC_results[8])
     M_range = numpy.arange(min(MC_int_M_array), max(MC_int_M_array) + bin_size + 1, bin_size)
     M_hist = numpy.histogram(MC_int_M_array, bins = M_range)
     M_hist_x = numpy.delete(M_hist[1], len(M_hist[1]) - 1, axis = None)
     M_hist_y = M_hist[0]
 
-    MC_int_E_array = numpy.rint(MC_results[8])
+    MC_int_E_array = numpy.rint(MC_results[9])
     E_range = numpy.arange(min(MC_int_E_array), max(MC_int_E_array) + bin_size + 1, bin_size)
     E_hist = numpy.histogram(MC_int_E_array, bins = E_range)
     E_hist_x = numpy.delete(E_hist[1], len(E_hist[1]) - 1, axis = None)
@@ -192,7 +221,7 @@ def MC_hist(grid, MC_steps, mag_mom, coupl_x, coupl_y, tymherr, bin_size, hist_t
     matplotlib.pyplot.bar(E_hist_x, E_hist_y)
     matplotlib.pyplot.show()
 
-    return (MC_results[7], MC_results[8], M_hist_x, M_hist_y, E_hist_x, E_hist_y)
+    return (MC_results[8], MC_results[9], M_hist_x, M_hist_y, E_hist_x, E_hist_y)
 
 
 # Here, we run the simulation. For testing, we also print the actual arrays; these commands are then commented out as necessary.
@@ -201,25 +230,18 @@ print "                      "
 print_grid(initial_grid)
 print "                      "
 print "                      "
-updated_grid = many_MC(array = initial_grid, MC_iter = 10, ext_field = 0.0, cc_x = 1.0, cc_y = 1.0, tepl = 2.0)
+updated_grid = many_MC(array = initial_grid, MC_iter = 10, ext_field = 0.0, cc_x = 1.0, cc_y = 1.0, tepl = 2.0, therm_steps_per_sample = 10, many_MC_G_dist = 1, many_MC_G_corr = False)
 print "Updated 2D Ising Grid:"
 print "                      "
 print_grid(updated_grid[0])
-output_1NN = MC_hist(initial_grid, MC_num, h_hist, Jx_hist, Jy_hist, T_hist, hist_bin_size, MC_therm_steps)
-
-'''print output_1NN[1]
-print output_1NN[4]
-print output_1NN[5]
-print len(output_1NN[1])
-print len(output_1NN[4])
-print len(output_1NN[5])'''
+output_1NN = MC_hist(initial_grid, MC_num, h_hist, Jx_hist, Jy_hist, T_hist, hist_bin_size, MC_therm_steps, kNN_2pt_G_dist, kNN_2pt_G_conn)
 
 # This section stores the time at the end of the program.
 program_end_time = time.clock()
 total_program_time = program_end_time - program_start_time
 print "                      "
 print "Program run time: %f seconds" % (total_program_time)
-print "Program run time per site per MC sweep: %6g seconds" % (total_program_time / (MC_num * sweeps * size))
+print "Program run time per site per MC sweep: %6g seconds" % (total_program_time / (MC_num * MC_therm_steps * size))
 
 ''' Note: To find out how long the program takes, we take the difference of time.clock() evaluated at
     the beginning of the program and at the end of the program. Here, we take the time at the end of
